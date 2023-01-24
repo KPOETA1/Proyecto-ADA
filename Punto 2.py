0ef22b1d1f5e32ed5bdc15dccf38a52cf6ce6765
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-01-24 10:28:31 -0500

    pruebas p1

--- Punto 2.py
+++ Punto 2.py
@@ -1,16 +1,25 @@
 import sys
 
 # Clase Arbol Trie
+# en caso de empate se retorna la palabra mayor palabra lexicograficamente
+
+# funcion para obtener el valor maximo de un diccionario
+def getMaxValue(d):
+    for key in d:
+        d[key] = d[key].number_of_words
+    sortedDict = dict(sorted(d.items()))
+    return max(sortedDict, key=d.get)
+
 class TrieNode:
     def __init__(self):
         self.children = {}
         self.number_of_words = 0
         self.isEnd = False
 
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-
 
     def insert(self, word):  # word: str
         current = self.root  # nodo actual
@@ -20,7 +29,6 @@ class Trie:
             current = current.children[c]  # se mueve al nodo hijo
             current.number_of_words += 1
         current.isEnd = True  # se marca como final de la palabra
-    
 
     def getWords(self, prefix):  # prefix: str
         current = self.root  # nodo actual
@@ -31,7 +39,16 @@ class Trie:
             current = current.children[c]  # se mueve al nodo hijo
         # retorna la lista de palabras
         return self.getWordsAux(current, prefix)
-    
+
+     # Funcion recursiva para obtener las palabras
+    def getWordsAux(self, node, prefix):    # node: TrieNode, prefix: str
+        if node.isEnd:  # si es el final de la palabra
+            return [prefix]  # retorna la palabra
+        words = []  # lista de palabras
+        for c in node.children:  # para cada hijo del nodo
+            words += self.getWordsAux(node.children[c], prefix + c)
+        return words  # retorna la lista de palabras
+
     # funcion para buscar segun la palabra cual es el prefijo mas grande
     def getPrefix(self, word):
         current = self.root
@@ -42,31 +59,57 @@ class Trie:
             current = current.children[c]
             prefix += c
         return prefix
-    
+
+    # contador de palabras con el mismo prefijo countPrefix
+    def countPrefix(self, prefix):
+        current = self.root
+        for c in prefix:
+            current = current.children[c]
+        return current.number_of_words
+
+    # funcion para recorrer el arbol
+    def recorrerArbol(self, prefix):
+        current = self.root
+        for c in prefix:
+            current = current.children[c]
+            # si hay empate se retorna la palabra mayor lexicograficamente
+        if not current.isEnd:
+            prefix += getMaxValue(current.children.copy())
+            return self.recorrerArbol(prefix)
+        else:
+            return prefix
+
+
+
+
+    # funcion para obtener la palabra mas grande
+
     def getBiggestWord(self, word):
         current = self.root
         prefix = self.getPrefix(word)
 
-        for c in prefix:
-            current = current.children[c]
+        prefix = self.recorrerArbol(prefix)
         
-        #print(current.number_of_words)
-        if current.number_of_words > 1:
-            count = 1
-            letter = ""
-            for c in current.children:
-                if current.children[c].number_of_words > count:
-                    count = current.children[c].number_of_words
-                    letter = c
-            prefix += letter
 
-        #print (prefix)
+        # for c in prefix:
+        #     current = current.children[c]
+
+        # if current.number_of_words > 1:
+        #     count = 1
+        #     letter = ""
+        #     for c in current.children:
+        #         if current.children[c].number_of_words > count:
+        #             count = current.children[c].number_of_words
+        #             letter = c
+        #     prefix += letter
+
+        # print (prefix)
         words = self.getWords(prefix)
         shortest = self.getShortestWord(words)
-        #return self.getShortestWord(words)
+        # return self.getShortestWord(words)
         return shortest
 
-
+    # funcion para obtener la palabra mas corta
     def getShortestWord(self, words):
         words.sort()
         shortest = words[0]
@@ -75,21 +118,6 @@ class Trie:
                 shortest = word
         return shortest
 
-    # Funcion recursiva para obtener las palabras
-    def getWordsAux(self, node, prefix):    # node: TrieNode, prefix: str
-        if node.isEnd:  # si es el final de la palabra
-            return [prefix]  # retorna la palabra
-        words = []  # lista de palabras
-        for c in node.children:  # para cada hijo del nodo
-            words += self.getWordsAux(node.children[c], prefix + c)
-        return words  # retorna la lista de palabras
-    
-    # contador de palabras con el mismo prefijo countPrefix
-    def countPrefix(self, prefix):
-        current = self.root
-        for c in prefix:
-            current = current.children[c]
-        return current.number_of_words
 
 def main():
     trie = Trie()  # crear arbol trie
@@ -109,12 +137,13 @@ def main():
         if query_type == 1:  # si es de tipo 1
             trie.insert(word)  # se inserta la palabra
         elif query_type == 2:  # si es de tipo 2
-            # for i in trie.getWords(word): 
+            # for i in trie.getWords(word):
             #     print(i)
-            #escribe en el archivo de salida
-            with open('PROYECTO/Proyecto-ADA/output.txt', 'a') as f:
-                f.write(str(trie.getBiggestWord(word)) + "\n")
-            #print(trie.getBiggestWord(word))
+            # escribe en el archivo de salida
+            # with open('PROYECTO/Proyecto-ADA/output.txt', 'a') as f:
+            #   f.write(str(trie.getBiggestWord(word)) + "\n")
+            print(trie.getBiggestWord(trie.getPrefix(word)))
+
 
 if __name__ == "__main__":
     main()
