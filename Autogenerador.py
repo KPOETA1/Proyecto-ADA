95dc8842e1a339e9d026e614755e68f429730671
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-01-27 13:38:49 -0500

    auto chiste

--- Autogenerador.py
+++ Autogenerador.py
@@ -11,18 +11,26 @@ def randomword(length):
     return ''.join(random.choice(letters) for i in range(length)) 
 
 # numero de palabras que se imprimen en el archivo
-max = 1000000 # maximo de palabras
+#max = 1000000 # maximo de palabras
 
 # tipo de palabra
 def randomtype():
     return random.randint(1,2)
 
-
-def main():
-    file = open("PROYECTO\Proyecto-ADA\words.txt", "w")
+def generar_problema(max):
+    path = 'input.txt'
+ 
+    try:
+        with open(path, 'r+') as f:
+            f.truncate()
+    except IOError:
+        print('Failure')
+    
+    file = open(path, "w")
     file.write(str(max) + "\n")
+    file.write(str(1) + " " + randomword(random.randint(2,20)) + "\n")
+
     for i in range(max):     
         file.write(str(randomtype()) + " " + randomword(random.randint(2,20)) + "\n")
-
-if __name__ == "__main__":
-    main()
+    
+    file.close()
